fe14ebce661a6e390a06b4adc9f7297dfe6e1bc9
mateus-brites <mateusmmo15@gmail.com>
2022-03-21 17:02:10 -0300

    trabalhando com parametros

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,9 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('courses') // tudo que cair na rota /courses vai ser tratado por esse controller
 export class CoursesController {
   @Get('list') // será chamado na requisição courses/list
   FindAll() {
     return 'All courses';
+  }
+
+  @Get(':id') // indica um parâmetro dinâmico ex: /1 ou /2...
+  FindOne(@Param() params) {
+    // params vira um array que recebe todos os parâmetros obs: pode ser desestruturado
+    return `Curso #${params.id}`;
   }
 }
