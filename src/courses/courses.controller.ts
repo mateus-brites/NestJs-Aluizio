d3ffe7bdec6a61dea57e6b9bbd63c569c498309d
mateus-brites <mateusmmo15@gmail.com>
2022-03-22 11:04:10 -0300

    trabalhando com statuscode

--- src/courses/courses.controller.ts
+++ src/courses/courses.controller.ts
@@ -1,10 +1,20 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  HttpCode,
+  HttpStatus,
+  Param,
+  Post,
+  Res,
+} from '@nestjs/common';
+import { response } from 'express';
 
 @Controller('courses') // tudo que cair na rota /courses vai ser tratado por esse controller
 export class CoursesController {
   @Get('list') // será chamado na requisição courses/list
-  FindAll() {
-    return 'All courses';
+  FindAll(@Res() response) {
+    return response.status(200).send('All courses');
   }
 
   @Get(':id') // indica um parâmetro dinâmico ex: /1 ou /2...
@@ -14,6 +24,7 @@ export class CoursesController {
   }
 
   @Post()
+  @HttpCode(HttpStatus.NO_CONTENT)
   Create(@Body() body) {
     // body é um objeto que contem todas as informações que estão sendo enviadas
     return body;
