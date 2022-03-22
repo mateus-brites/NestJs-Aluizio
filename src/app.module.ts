87e0abb9f6456e5b42e28b314533a1ec2aa7033d
mateus-brites <mateusmmo15@gmail.com>
2022-03-22 15:18:28 -0300

    trabalhando com modulo

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,11 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoursesModule } from './courses/courses.module';
-import { CoursesService } from './courses/courses.service';
 
 @Module({
   imports: [CoursesModule],
   controllers: [AppController],
-  providers: [AppService, CoursesService],
+  providers: [AppService],
 })
 export class AppModule {}
